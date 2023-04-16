5c55596339a25ae714d68b874d075126b6d1f589
高木真由 <87755330+Mayu0628@users.noreply.github.com>
2023-04-16 23:52:56 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,84 @@
+// 曜日を格納
+const week = ["日", "月", "火", "水", "木", "金", "土"];
+
+// Dateインスタンスを作成
+const today = new Date();
+// 月末だとずれる可能性があるため、1日固定で取得
+var showDate = new Date(today.getFullYear(), today.getMonth(), 1);
+
+// 初期表示　HTMLが読み込まれた直後に呼び出される関数
+window.onload = function () {
+    showProcess(today, calendar);
+};
+
+// 前の月表示
+function prev(){
+    showDate.setMonth(showDate.getMonth() - 1);
+    showProcess(showDate);
+}
+
+// 次の月表示
+function next(){
+    showDate.setMonth(showDate.getMonth() + 1);
+    showProcess(showDate);
+}
+
+// カレンダー表示　おおもと
+function showProcess(date) {
+    var year = date.getFullYear();
+    var month = date.getMonth();
+    // getMonth()は0始まりのため(month + 1) する。
+    document.querySelector('#header').innerHTML = year + "年 " + (month + 1) + "月";
+
+    var calendar = createProcess(year, month);
+    document.querySelector('#calendar').innerHTML = calendar;
+}
+
+// カレンダー作成
+function createProcess(year, month) {
+    // 曜日
+    var calendar = "<table><tr class='dayOfWeek'>";
+    for (var i = 0; i < week.length; i++) {
+        calendar += "<th>" + week[i] + "</th>";
+    }
+    calendar += "</tr>";
+
+    // 日付のカウント
+    var count = 0;
+    // 表示する月の1日の曜日
+    var startDayOfWeek = new Date(year, month, 1).getDay();
+    // 表示する月の末日
+    var endDate = new Date(year, month + 1, 0).getDate();
+    // 表示する先月の末日
+    var lastMonthEndDate = new Date(year, month, 0).getDate();
+    // カレンダー（日付部分）の行数
+    var row = Math.ceil((startDayOfWeek + endDate) / week.length);
+
+    // 1行ずつ設定
+    for (var i = 0; i < row; i++) {
+        calendar += "<tr>";
+        // 1colum単位で設定
+        for (var j = 0; j < week.length; j++) {
+            if (i == 0 && j < startDayOfWeek) {
+                // 1行目で1日まで先月の日付を設定
+                calendar += "<td class='disabled'>" + (lastMonthEndDate - startDayOfWeek + j + 1) + "</td>";
+            } else if (count >= endDate) {
+                // 最終行で最終日以降、翌月の日付を設定
+                count++;
+                calendar += "<td class='disabled'>" + (count - endDate) + "</td>";
+            } else {
+                // 当月の日付を曜日に照らし合わせて設定
+                count++;
+                if(year == today.getFullYear()
+                    && month == (today.getMonth())
+                    && count == today.getDate()){
+                    calendar += "<td class='today'>" + count + "</td>";
+                } else {
+                    calendar += "<td>" + count + "</td>";
+                }
+            }
+        }
+        calendar += "</tr>";
+    }
+    return calendar;
+}
